a8d762bca345f81152d9c34250c19fabeaff16e0
johnpaul <edehjohnpaul@gmail.com>
2022-05-19 16:15:25 -0700

    ok

--- NodeMySql/app.js
+++ NodeMySql/app.js
@@ -11,25 +11,25 @@ dotenv.config({ path: './.env' });
 // middle wares
 app.use(morgan('dev'))
 
-// const db = mysql.createConnection ({
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE
-// });
+ const db = mysql.createConnection ({
+     host: process.env.DATABASE_HOST,
+     user: process.env.DATABASE_USER,
+     password: process.env.DATABASE_PASSWORD,
+     database: process.env.DATABASE
+});
 
 const publicDirectory = path.join(__dirname, './public', '../..' );
 app.use(express.static(publicDirectory));
 
 app.set('view engine', 'ejs');
-// app.set('views', './view');
-// db.connect( (error) => {
-//     if(error) {
-//         console.log(error)
-//     }else {
-//         console.log("my sql connected")
-//     }
-// })
+ app.set('views', './view');
+ db.connect( (error) => {
+    if(error) {
+        console.log(error)
+     }else {
+        console.log("my sql connected")
+    }
+})
 app.get('/', (req, res) => {
 //    res.send("<h1>homepage</h1>")
    res.render('index');
